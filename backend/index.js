d7a8f5815654e90d48db0aed0908170339328bdb
ashwinkothavade <ashwinkothavade@gmail.com>
2025-09-26 10:56:30 +0530

    Changes in api endpoints

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,12 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on ${PORT}`));
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [/\.vercel\.app$/, 'http://localhost:3000'],
+  credentials: false
+}));
 app.use(express.json());
 
 // API routes
